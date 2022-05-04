05af708ef3d8f6e990231617758638a4238badd5
vik-ok-sova <104892791+vik-ok-sova@users.noreply.github.com>
2022-05-04 21:30:06 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,132 @@
+/*import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+  We use the same configuration as Parcel to bundle this sandbox, you can find more
+  info about Parcel 
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+`;
+*/
+var data = [];
+$("#tlacitko").click(function () {
+  var cisla = [
+    Math.floor(Math.random() * 6) + 1,
+    Math.floor(Math.random() * 6) + 1
+  ];
+  //alert($("#data").html().trim());
+  $("#vystup").html(cisla.join(", "));
+  $(
+    "#".concat(cisla.reduce((soucet, kostka) => (soucet += kostka)).toString())
+  ).html(function (n, current) {
+    return Number(current) + 1;
+  });
+  chart.updateSeries([
+    {
+      data: [
+        $("#2").text().trim(),
+        $("#3").text().trim(),
+        $("#4").text().trim(),
+        $("#5").text().trim(),
+        $("#6").text().trim(),
+        $("#7").text().trim(),
+        $("#8").text().trim(),
+        $("#9").text().trim(),
+        $("#10").text().trim(),
+        $("#11").text().trim(),
+        $("#12").text().trim()
+      ]
+    }
+  ]);
+});
+
+$("#tlacitko").click(function () {
+  $("#pokec").html(
+    "Zdejší randomizační prográmek ti zvrací tyto dvě kostečné hodnoty:"
+  );
+});
+
+var options = {
+  series: [
+    {
+      name: "Inflation",
+      data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    }
+  ],
+  chart: {
+    height: 350,
+    type: "bar"
+  },
+  plotOptions: {
+    bar: {
+      borderRadius: 10,
+      dataLabels: {
+        position: "bottom" // top, center, bottom
+      }
+    }
+  },
+  dataLabels: {
+    enabled: true,
+    formatter: function (val) {
+      return val;
+    },
+    offsetY: -20,
+    style: {
+      fontSize: "12px",
+      colors: ["#304758"]
+    }
+  },
+
+  xaxis: {
+    categories: ["2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"],
+    position: "top",
+    axisBorder: {
+      show: false
+    },
+    axisTicks: {
+      show: false
+    },
+    crosshairs: {
+      fill: {
+        type: "gradient",
+        gradient: {
+          colorFrom: "#D8E3F0",
+          colorTo: "#BED1E6",
+          stops: [0, 100],
+          opacityFrom: 0.4,
+          opacityTo: 0.5
+        }
+      }
+    },
+    tooltip: {
+      enabled: true
+    }
+  },
+  yaxis: {
+    axisBorder: {
+      show: false
+    },
+    axisTicks: {
+      show: false
+    },
+    labels: {
+      show: false,
+      formatter: function (val) {
+        return val + "%";
+      }
+    }
+  },
+  title: {
+    text: "Pekny grafik",
+    floating: true,
+    offsetY: 330,
+    align: "center",
+    style: {
+      color: "#444"
+    }
+  }
+};
+
+var chart = new ApexCharts(document.querySelector("#grafik"), options);
+chart.render();
